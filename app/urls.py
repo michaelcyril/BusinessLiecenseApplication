7c2d115ee0392792a22a990f96d432ea9c121563
DEOGASS WIDAMBE <87471672+widambeDeograss@users.noreply.github.com>
2023-05-11 13:03:40 +0300

    front&end

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', index, name='index'),
-    path('/ombi_jipya', start_request, name='start'),
-    path('/endeleza_ombi', request_details),
-    path('/fuatilia_ombi', request_followup, name='followup')
+    path('ombi_jipya', start_request, name='start'),
+    path('endeleza_ombi', request_details),
+    path('fuatilia_ombi', request_followup, name='followup')
 ]
--- main_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-
-model = [User, TraInformation, AddressInformation]
-for data in model:
-    admin.site.register(data)
